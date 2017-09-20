26b7b10c435b591deeb3bb84824cb6c843031403
Kunle Adeyinka <aadeyink@gmail.com>
2017-09-19 22:00:06 -0400

    adding items to the cart

--- layout_foot.php
+++ layout_foot.php
@@ -5,10 +5,23 @@
         <!-- /container -->
 
         <!-- jQuery library -->
-        <script src="libs/js/jquery.js"></script>
+        <script src="libs/js/jquery.min.js"></script>
 
         <!-- bootstrap JavaScript -->
         <script src="libs/js/bootstrap.min.js"></script>
+
+        <script>
+          $(document).ready(function(){
+            //add to cart button listener
+            $('.add-to-cart-form').on('submit', function(){
+              var id = $(this).find('.product-id').text();
+              var quantity = $(this).find('.cart-quantity').val();
+
+              window.location.href="add_to_cart.php?id=" + id + "&quantity=" + quantity;
+              return false;
+            });
+          });
+        </script>
 
 </body>
 </html>
